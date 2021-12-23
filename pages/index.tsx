82a5cff9d7ba740f9e6344d71131d791e3ce2d49
Max Malm <benjick@dumfan.net>
2021-12-23 23:26:09 +0100

    Add cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import { SortKey, useGames, useStore } from "../src/state";
 import { Game } from "../functions/src";
 import { Filter } from "../components/Filter";
@@ -36,65 +35,78 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>XBOX gamepass app</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Filter />
-      <table className="table-auto w-100">
-        <thead>
-          <tr>
-            <th>Image</th>
-            <HeaderItem id="name">Game</HeaderItem>
-            <HeaderItem id="opencritic.percentRecommended">
-              Recommended
-            </HeaderItem>
-            <HeaderItem id="opencritic.averageScore">Average score</HeaderItem>
-            <HeaderItem id="opencritic.medianScore">Median score</HeaderItem>
-            <th>Source</th>
-            <th>Hide</th>
-          </tr>
-        </thead>
-        <tbody>
-          {games.map((game: Game) => (
-            <tr key={game.id}>
-              <td>
+      <div className="container">
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-6"
+        >
+          {games.map((game: Game) => {
+            const image = game.images.find(
+              (image) => image.purpose === "SuperHeroArt"
+            )?.url;
+            return (
+              <li
+                key={game.id}
+                className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
+              >
                 <img
-                  src={
-                    game.images.find(
-                      (image) => image.purpose === "SuperHeroArt"
-                    )?.url
-                  }
-                  style={{ width: 100 }}
-                  alt="game"
+                  className="w-100 flex-shrink-0 mx-auto flex-grow"
+                  src={image}
+                  alt=""
                 />
-              </td>
-              <td>{game.name}</td>
-              <td>{round(game.opencritic.percentRecommended)}%</td>
-              <td>{round(game.opencritic.averageScore)}%</td>
-              <td>{round(game.opencritic.medianScore)}%</td>
-              <td>
-                {game.source === "xbox-gamepass" ? "Gamepass" : "EA Play"}
-              </td>
-              <td>
-                <button
-                  type="button"
-                  className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  onClick={() => hideGame(game.id)}
-                >
-                  <XCircleIcon
-                    className="-ml-0.5 mr-2 h-4 w-4"
-                    aria-hidden="true"
-                  />
-                  Hide
-                </button>
-              </td>
-            </tr>
-          ))}
-          <tr></tr>
-        </tbody>
-      </table>
+                <div className="flex-1 flex flex-col p-8">
+                  <h3 className="text-gray-900 text-sm font-medium">
+                    {game.name}
+                  </h3>
+                  <dl className="mt-1 flex-grow flex flex-col justify-between">
+                    <dt className="sr-only">Title</dt>
+                    <dd className="text-gray-500 text-sm">{game.category}</dd>
+                    <dt className="sr-only">Category</dt>
+                    <dd className="mt-3">
+                      <span className="px-2 py-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+                        {round(game.opencritic.percentRecommended)}%
+                      </span>
+                      <span className="px-2 py-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+                        {round(game.opencritic.averageScore)}%
+                      </span>
+                      <span className="px-2 py-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+                        {round(game.opencritic.medianScore)}%
+                      </span>
+                      <span className="px-2 py-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+                        {game.source === "xbox-gamepass"
+                          ? "Gamepass"
+                          : "EA Play"}
+                      </span>
+                    </dd>
+                  </dl>
+                </div>
+                <div>
+                  <div className="-mt-px flex divide-x divide-gray-200">
+                    <div className="w-0 flex-1 flex">
+                      <button
+                        onClick={() => hideGame(game.id)}
+                        className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
+                      >
+                        <XCircleIcon
+                          className="w-5 h-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                        <span className="ml-3">Hide</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
